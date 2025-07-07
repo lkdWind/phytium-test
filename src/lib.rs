4f2a754ea936d6b0ce593a9cab17c37404729c84
周睿 <zrufo747@outlook.com>
2025-07-07 21:06:51 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 #![no_std]
 
+use core::ptr::NonNull;
+
 extern crate alloc;
+
+pub struct Pl011{
+   pub  base_addr: NonNull<u8>,
+}
+
+
+
+impl Pl011 {
+    pub fn new(base_addr: NonNull<u8>) -> Self {
+        Self { base_addr }
+    }
+}
--- tests/test.rs
+++ tests/test.rs
@@ -7,15 +7,38 @@ extern crate bare_test;
 
 #[bare_test::tests]
 mod tests {
-    use bare_test::println;
+    use bare_test::{
+        globals::{PlatformInfoKind, global_val},
+        mem::iomap,
+        println,
+    };
     use log::info;
+    use pl011::Pl011;
 
     #[test]
     fn it_works() {
         info!("This is a test log message.");
-        let a = 2;
-        let b = 2;
-        assert_eq!(a + b, 4);
+
+        let PlatformInfoKind::DeviceTree(fdt) = &global_val().platform_info;
+        let fdt = fdt.get();
+
+        let node = fdt.find_compatible(&["arm,pl011"]).next().unwrap();
+
+        let reg = node.reg().unwrap().next().unwrap();
+
+        let base = reg.address;
+
+        let mmio = iomap((base as usize).into(), reg.size.unwrap());
+
+        let pl011 = Pl011 { base_addr: mmio };
+
+        println!("PL011 base address: {:p}", pl011.base_addr);
+
+        unsafe { pl011.base_addr.as_ptr().write_volatile(b'A') }; // Write 'A' to the data register}
+
+        unsafe { pl011.base_addr.as_ptr().write_volatile(b'\r') }; // Write 'A' to the data register}
+        unsafe { pl011.base_addr.as_ptr().write_volatile(b'\n') }; // Write 'A' to the data register}
+
         println!("test passed!");
     }
 }
